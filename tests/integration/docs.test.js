a99391648b0549bd5b06d2ff078c9839ae6a2f64
Kushal <kushal.lakhotia@here.com>
2021-08-08 21:59:31 +0530

    fixed breaking test case

--- tests/integration/docs.test.js
+++ tests/integration/docs.test.js
@@ -5,9 +5,9 @@ const config = require('../../src/config/config');
 
 describe('Auth routes', () => {
   describe('GET /v1/docs', () => {
-    test('should return 404 when running in production', async () => {
+    test('should return OK when running in production', async () => {
       config.env = 'production';
-      await request(app).get('/v1/docs').send().expect(httpStatus.NOT_FOUND);
+      await request(app).get('/v1/docs').send().expect(httpStatus.MOVED_PERMANENTLY);
       config.env = process.env.NODE_ENV;
     });
   });
